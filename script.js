9585bd6679f3642ebdb8b27f282e05c3619b90cf
analiria <pires.portes@escola.pr.gov.br>
2025-10-22 10:22:25 -0300

    viva.pitanga

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de comportamento do formulário
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Obrigado pelo contato! Em breve entraremos em contato.");
+});
